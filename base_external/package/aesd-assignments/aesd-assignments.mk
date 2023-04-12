cfaadd9227b15b933cfc7fe7b883223216cc8288
juanisimomg <jugo6138@colorado.edu>
2023-04-12 00:41:29 -0700

    Updating configs and make files

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 # DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '96d23d25ef1f9b9f7ebd526a6fc947b817f0390a'
+AESD_ASSIGNMENTS_VERSION = 'b59173f3c7558839f4c70d72b583a26e572f39a5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
