2f0d7d27fea1f3a70cd84e5398c49ff8e73822a1
juanisimomg <jugo6138@colorado.edu>
2023-04-14 11:51:04 -0700

    Added support for syslog in QEMU

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 # DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3a15cee5467e757cba07a1cc8fc035d06d02b350'
+AESD_ASSIGNMENTS_VERSION = 'cea19747c34335dbd80b6b3d25de4afc499fd6fb'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
